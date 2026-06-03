b0ec1bf296e7673f2cc63ebf41a363761af9a386
yiling <njjyl723@gmail.com>
2026-06-03 23:48:51 +0800

    fix(webui): set ScrollArea to min-h-0 in AgentFormDialog to allow scrolling

--- packages/webui/components/settings/AgentFormDialog.tsx
+++ packages/webui/components/settings/AgentFormDialog.tsx
@@ -230,7 +230,7 @@ export function AgentFormDialog({
           }}
           className="flex-1 flex flex-col min-h-0 overflow-hidden"
         >
-          <ScrollArea className="flex-1 h-full">
+          <ScrollArea className="flex-1 min-h-0">
             <div className="p-6 pt-2 space-y-6">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <form.Field
