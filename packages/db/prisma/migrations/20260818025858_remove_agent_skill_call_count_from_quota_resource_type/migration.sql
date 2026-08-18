83b57bed8ed9f4513ed21da3d338d0e154aad9b1
yiling <njjyl723@gmail.com>
2026-08-18 11:05:06 +0800

    chore(db): replace manual migration with prisma-generated migration

--- packages/db/prisma/migrations/20260818025858_remove_agent_skill_call_count_from_quota_resource_type/migration.sql
+++ packages/db/prisma/migrations/20260818025858_remove_agent_skill_call_count_from_quota_resource_type/migration.sql
@@ -1,8 +1,14 @@
+/*
+  Warnings:
+
+  - The values [agent_skill_call_count] on the enum `QuotaResourceType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
 -- AlterEnum
 BEGIN;
 CREATE TYPE "QuotaResourceType_new" AS ENUM ('agent_total_tokens', 'agent_cost', 'agent_mcp_call_count', 'agent_bash_call_count', 'agent_tool_call_count');
 ALTER TABLE "quota_rules" ALTER COLUMN "resource" TYPE "QuotaResourceType_new" USING ("resource"::text::"QuotaResourceType_new");
 ALTER TYPE "QuotaResourceType" RENAME TO "QuotaResourceType_old";
 ALTER TYPE "QuotaResourceType_new" RENAME TO "QuotaResourceType";
-DROP TYPE "QuotaResourceType_old";
+DROP TYPE "public"."QuotaResourceType_old";
 COMMIT;
